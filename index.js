edb75a667c4325f1f030dbb9b911c3f6a95721d9
fridiValach <pridav@webschool.co.il>
2022-05-16 22:02:25 +0300

    forth commit

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ mongoose.connect(
   "mongodb+srv://newHW4:newHW4@cluster0.amuqt.mongodb.net/?retryWrites=true&w=majority"
 );
 
+require("dotenv").config()
+
 const express = require("express");
 const app = express();
 
 const AccountSchema = require("./AccountSchema");
 const ActionSchema = require("./ActionSchema");
 
-const PORT = 5555;
+//const PORT = 5555;
 
 app.use(express.json());
 
@@ -22,11 +24,12 @@ async function findAccounts(typeSchema, req, res, id) {
         list+=`<li>${entry}</li>`
       })
   });
+  console.log(accountsList);
     res.send(accountsList);
 }
 
-app.get("/api/accounts", (req, res) => {
-    findAccounts(AccountSchema, req, res, {})
+app.get("/api/accounts",async (req, res) => {
+    await findAccounts(AccountSchema, req, res, {})
 });
 
 async function saveAccount(req, res, next) {
@@ -41,18 +44,18 @@ app.post("/api/accounts", saveAccount, (req, res) => {
   res.send(`id ${newAccount.id} added succsesfully`);
 });
 
-app.get("/api/accounts/:id", (req, res) => {
+app.get("/api/accounts/:id",async (req, res) => {
   const newAccount = new AccountSchema(req.body);
-  findAccounts(AccountSchema, req, res, {id:req.params.id});
+  await findAccounts(AccountSchema, req, res, {id:req.params.id});
 });
 
-app.get("/api/actions", (req, res) => {
-  findAccounts(ActionSchema, req, res, {});
+app.get("/api/actions",async (req, res) => {
+  await findAccounts(ActionSchema, req, res, {});
 });
-
+/*
 async function updateAccount(req, res, next, id, key, newValue) {
      await AccountSchema.findOneAndUpdate(
-    { id: id },
+    { id },
     { [key]: newValue }
   );
   next();
@@ -63,7 +66,7 @@ function postFunc(rout) {
     (req, res, next) => {
       const key = "";
       rout === "credit" ? (key = credit) : (key = isActive);
-      updateAccount(req, res, next, req.params.id, key, req.body.credit);
+      updateAccount(req, res, next, body.id, key, req.body.credit);
     },
     (req, res) => {
       res.send(`id ${req.params.id} updated succsesfully`);
@@ -72,7 +75,21 @@ function postFunc(rout) {
 }
 
 postFunc("credit");
-postFunc("active");
+postFunc("active");*/
+
+app.post(
+    "/api/accounts/:id",
+    async    (req, res, next) => {
+        let body=req.body
+      body.active?body["active"]=body["isActive"]:
+      await AccountSchema.findOneAndUpdate(
+        { id:req.params.id },body);
+      next();
+    },
+    (req, res) => {
+      res.send(`id ${req.params.id} updated succsesfully`);
+    }
+  );
 
 async function depositFunc(req, res, next) {
   new ActionSchema({ ...req.body, actionType: "deposit" });
@@ -88,12 +105,17 @@ app.post("/api/actions/:id/deposit", depositFunc, (req, res) => {
 
 async function withdrawFunc(req, res, next) {
   new ActionSchema({ ...req.body, actionType: "withdraw" });
+  await AccountSchema.findOneAndUpdate(
+    { id: req.body.account },
+    { cash: parseInt(req.body.amount) }
+  );
   next();
 }
 app.post("/api/actions/:id/withdraw", withdrawFunc, (req, res) => {
   res.send(`id ${req.params.id} withdrawed succsesfully`);
 });
+
 console.log(mongoose.connection.readyState);
 
-app.listen(PORT);
+app.listen(process.env.PORT);
 
